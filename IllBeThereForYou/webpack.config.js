1f0bf1a85b1ce24e35ae4a1a9b701570b1bcd536
darwin <oleg.umarov@gmail.com>
2016-06-08 22:33:44 -0500

    add missing dependencies

--- IllBeThereForYou/webpack.config.js
+++ IllBeThereForYou/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3001',
     'webpack/hot/only-dev-server',
-    './src/index',
+    './client/index',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
